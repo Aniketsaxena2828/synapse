3d0d84ce38239fdb775e23ad5505e40eef637d2e
Aniket <aniket07jan@gmail.com>
2026-05-21 01:39:54 +0530

    fixed frontend backend connection

--- server/src/config/socket.ts
+++ server/src/config/socket.ts
@@ -8,7 +8,10 @@ export const initSocket = (
 
   io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: [
+        "http://localhost:5173",
+        "https://anisynapse.netlify.app",
+      ],
       methods: ["GET", "POST"],
     },
   })
