d3b2a6d110fb8e79c999876dca2b4405079f85f7
Aniket <aniket07jan@gmail.com>
2026-05-21 03:30:51 +0530

    for the time being fixed backend connection

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  baseURL: "https://synapse-mx36.onrender.com/api",
   withCredentials: true,
 })
 
